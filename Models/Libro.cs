fa7496b883e6a70c4eed00e5c40cdea5e03d2743
Miguel-0510 <miguelangelbuitrago0510@gmail.com>
2026-09-19 18:32:08 -0500

    Panel de detalle: estrellas, reseña y mover de estante

---
+++ Models/Libro.cs
@@ -0,0 +1,18 @@
+﻿namespace Otium.Models;
+
+public enum Estante
+{
+    Leyendo,
+    Leido,
+    QuieroLeer
+}
+
+public class Libro
+{
+    public int Id { get; set; }
+    public string Titulo { get; set; } = "";
+    public string Autor { get; set; } = "";
+    public Estante Estante { get; set; }
+    public int Puntuacion { get; set; }   // 0 = sin puntuar, de 1 a 5
+    public string Resena { get; set; } = "";
+}
